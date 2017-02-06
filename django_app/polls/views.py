01fd9a4c786954f76cf222475227c2c48e6c39d8
cdragon-1 <streetfood89@gmail.com>
2017-02-06 18:02:23 +0900

    shortcut 만들기

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+
 from .models import Question
 
 
@@ -7,24 +8,29 @@ def index(request):
     # pub_date컬럼을 기준으로 내림차순 정렬한 결과를 latest_question_list 에 할당
     latest_question_list = Question.objects.order_by('-pub_date')
     # 돌려줄 문자열을 작성
-    output = ', '.join([q.question_text for q in latest_question_list])
+    # output = ', '.join([q.question_text for q in latest_question_list])
 
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
 
-    # output2 = ''
-    # for q in latest_question_list:
-    #     output2 += q.question_text + ', '
-    # output2 = output2[:2]
-    return HttpResponse(template.render(context, request))
-    # return HttpResponse(output)
-
+    return render(request, 'polls/index.html', context)
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    #
+    # # output2 = ''
+    # # for q in latest_question_list:
+    # #     output2 += q.question_text + ', '
+    # # output2 = output2[:2]
+    # return HttpResponse(template.render(context, request))
+    # # return HttpResponse(output)
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
 
 def result(request, question_id):
     response = "You're looking at the results of question %s"
