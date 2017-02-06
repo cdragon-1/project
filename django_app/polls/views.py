4aba3e56394d0bfec4a0a1eca93fa61afbb4db30
cdragon-1 <streetfood89@gmail.com>
2017-02-06 17:40:22 +0900

    index view 에서 Qeustion리스트를 출력

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    # pub_date컬럼을 기준으로 내림차순 정렬한 결과를 latest_question_list 에 할당
+    latest_question_list = Question.objects.order_by('-pub_date')
+    # 돌려줄 문자열을 작성
+    output = ', '.join([q.question_text for q in latest_question_list])
+
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    # output2 = ''
+    # for q in latest_question_list:
+    #     output2 += q.question_text + ', '
+    # output2 = output2[:2]
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse(output)
+
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s question_id")
-
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def result(request, question_id):
     response = "You're looking at the results of question %s"
@@ -15,4 +32,4 @@ def result(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question)
+    return HttpResponse("You're voting on question %s." % question_id)
